673e1fcb8c1c9e408237d4be95b4a5a589eb83c8
ruka charo <ruka.and.little.charo1@gmail.com>
2021-07-21 09:41:24 +0900

    Study_day3

--- chapter5.py
+++ chapter5.py
@@ -5,11 +5,20 @@ os.chdir('/Users/rukaoide/Library/Mobile Documents/com~apple~CloudDocs/Documents
 
 # いつもの設定のインポート
 from preamble import *
+import pandas as pd
+np.set_printoptions(precision=3)
+pd.set_option('display.max_columns', 500)
 
-from sklearn.datasets import load_iris, make_blobs
+from sklearn.datasets import load_iris, make_blobs, load_digits
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import \
-train_test_split, LeaveOneOut, cross_val_score, ShuffleSplit, GroupKFold
+train_test_split, LeaveOneOut, cross_val_score, ShuffleSplit, GroupKFold,\
+GridSearchCV
+from sklearn.metrics import classification_report, precision_recall_curve,\
+average_precision_score, f1_score, roc_curve, roc_auc_score, accuracy_score,\
+confusion_matrix
 
 #%% ------------------------------------
 '''5.1 交差検証'''
@@ -40,3 +49,249 @@ X, y = make_blobs(n_samples=12, random_state=0)
 groups = [0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 3]
 scores = cross_val_score(logreg, X, y, groups=groups, cv=GroupKFold(n_splits=3))
 scores
+
+
+#%% ------------------------------------
+'''5.2 グリッドサーチ'''
+# sklearnを用いたグリッドサーチ
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, random_state=0)
+
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
+              'gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
+grid_search = GridSearchCV(SVC(), param_grid, cv=5, return_train_score=True)
+
+grid_search.fit(X_train, y_train) # グリッドサーチの実行
+grid_search.score(X_test, y_test) # サーチ後のモデルの評価
+
+grid_search.best_params_ # ベストパラメータ
+grid_search.best_score_ # 交差検証の精度
+grid_search.best_estimator_ # ベストモデルへのアクセス
+
+#%% グリッドサーチの結果を表示する
+results = pd.DataFrame(grid_search.cv_results_)
+display(results.head())
+
+#%% 結果の可視化
+scores = np.array(results.mean_test_score).reshape(6, 6)
+mglearn.tools.heatmap(scores, xlabel='gamma', xticklabels=param_grid['gamma'],
+                      ylabel='C', yticklabels=param_grid['C'], cmap="coolwarm")
+
+
+#%% グリッドではないサーチ空間
+# リストで区切ればそれぞれ独立にサーチする
+param_grid = [{'kernel': ['rbf'],
+                'C': [0.001, 0.01, 0.1, 1, 10, 100],
+                'gamma': [0.001, 0.01, 0.1, 1, 10, 100]},
+              {'kernel': ['linear'],
+                'C': [0.001, 0.01, 0.1, 1, 10, 100]}]
+grid_search = GridSearchCV(SVC(), param_grid, cv=5, return_train_score=True)
+
+grid_search.fit(X_train, y_train)
+grid_search.score(X_test, y_test)
+
+grid_search.best_params_
+grid_search.best_score_
+
+
+#%% ネストした交差検証
+# modelにGridSearchCVを入れれば良い
+scores = cross_val_score(GridSearchCV(SVC(), param_grid, cv=5),
+                        iris.data, iris.target, cv=5)
+
+scores # バリデーションスコア
+scores.mean()
+
+
+#%% ------------------------------------
+'''5.3 評価基準とスコア'''
+# 2クラス分類における基準
+digits = load_digits()
+y = digits.target == 9
+
+X_train, X_test, y_train, y_test = train_test_split(
+    digits.data, y, random_state=0)
+
+# 学習と予測
+logreg = LogisticRegression(max_iter=1000, C=0.1).fit(X_train, y_train)
+pred_logreg = logreg.predict(X_test)
+print("logreg score: {:.2f}".format(logreg.score(X_test, y_test)))
+
+# classification_report
+print(classification_report(y_test, pred_logreg, target_names=['not nine', 'nine']))
+
+
+#%% 適合率ー再現率カーブとROCカーブ
+svc = SVC(gamma=0.05).fit(X_train, y_train)
+precision, recall, thresholds = precision_recall_curve(
+    y_test, svc.decision_function(X_test))
+
+# 得られたデータをもとにグラフを書く
+X, y = make_blobs(n_samples=(4000, 500), cluster_std=[7.0, 2], random_state=22)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+svc = SVC(gamma=.05).fit(X_train, y_train)
+precision, recall, thresholds = precision_recall_curve(
+    y_test, svc.decision_function(X_test))
+# ゼロに最も近いスレッショルドを探す
+close_zero = np.argmin(np.abs(thresholds))
+plt.plot(precision[close_zero], recall[close_zero], 'o', markersize=10,
+         label="threshold zero", fillstyle="none", c='k', mew=2)
+
+plt.plot(precision, recall, label="precision recall curve")
+plt.xlabel("Precision")
+plt.ylabel("Recall")
+plt.legend(loc="best")
+
+
+#%% クラス分類器によって作動ポイントは異なる
+rf = RandomForestClassifier(n_estimators=100, random_state=0, max_features=2)
+rf.fit(X_train, y_train)
+
+# predict_probaでサンプルが「1」になる確率を渡す
+precision_rf, recall_rf, thresholds_rf = precision_recall_curve(
+    y_test, rf.predict_proba(X_test)[:, 1])
+
+plt.plot(precision, recall, label="svc")
+
+plt.plot(precision[close_zero], recall[close_zero], 'o', markersize=10,
+         label="threshold zero svc", fillstyle="none", c='k', mew=2)
+
+plt.plot(precision_rf, recall_rf, label="rf")
+
+close_default_rf = np.argmin(np.abs(thresholds_rf - 0.5))
+plt.plot(precision_rf[close_default_rf], recall_rf[close_default_rf], '^', c='k',
+         markersize=10, label="threshold 0.5 rf", fillstyle="none", mew=2)
+plt.xlabel("Precision")
+plt.ylabel("Recall")
+plt.legend(loc="best")
+
+print("f1_score of random forest: {:.3f}".format(
+    f1_score(y_test, rf.predict(X_test))))
+print("f1_score of svc: {:.3f}".format(f1_score(y_test, svc.predict(X_test))))
+
+#%% 適合率ー再現率カーブの積分である平均適合率
+ap_rf = average_precision_score(y_test, rf.predict_proba(X_test)[:, 1])
+ap_svc = average_precision_score(y_test, svc.decision_function(X_test))
+print("Average precision of random forest: {:.3f}".format(ap_rf))
+print("Average precision of svc: {:.3f}".format(ap_svc))
+
+
+#%% ROCカーブ
+fpr, tpr, thresholds = roc_curve(y_test, svc.decision_function(X_test))
+
+plt.plot(fpr, tpr, label="ROC Curve")
+plt.xlabel("FPR")
+plt.ylabel("TPR (recall)")
+# ゼロに最も近いスレッショルドを見つける
+close_zero = np.argmin(np.abs(thresholds))
+plt.plot(fpr[close_zero], tpr[close_zero], 'o', markersize=10,
+         label="threshold zero", fillstyle="none", c='k', mew=2)
+plt.legend(loc=4)
+
+
+#%% ランダムフォレストとSVCの比較
+fpr_rf, tpr_rf, thresholds_rf = roc_curve(y_test, rf.predict_proba(X_test)[:, 1])
+
+plt.plot(fpr, tpr, label="ROC Curve SVC")
+plt.plot(fpr_rf, tpr_rf, label="ROC Curve RF")
+
+plt.xlabel("FPR")
+plt.ylabel("TPR (recall)")
+plt.plot(fpr[close_zero], tpr[close_zero], 'o', markersize=10,
+         label="threshold zero SVC", fillstyle="none", c='k', mew=2)
+close_default_rf = np.argmin(np.abs(thresholds_rf - 0.5))
+plt.plot(fpr_rf[close_default_rf], tpr[close_default_rf], '^', markersize=10,
+         label="threshold 0.5 RF", fillstyle="none", c='k', mew=2)
+
+plt.legend(loc=4)
+
+
+#%% ROCカーブの積分であるAUC
+rf_auc = roc_auc_score(y_test, rf.predict_proba(X_test)[:, 1])
+svc_auc = roc_auc_score(y_test, svc.decision_function(X_test))
+print("AUC for Random Forest: {:.3f}".format(rf_auc))
+print("AUC for SVC: {:.3f}".format(svc_auc))
+
+
+#%% 偏ったデータセットでのROCカーブとAUC
+y = digits.target == 9
+
+X_train, X_test, y_train, y_test = train_test_split(
+    digits.data, y, random_state=0)
+
+plt.figure()
+
+for gamma in [1, 0.05, 0.01]:
+    svc = SVC(gamma=gamma).fit(X_train, y_train)
+    accuracy = svc.score(X_test, y_test)
+    auc = roc_auc_score(y_test, svc.decision_function(X_test))
+    fpr, tpr, _ = roc_curve(y_test , svc.decision_function(X_test))
+    print("gamma = {:.2f}  accuracy = {:.2f}  AUC = {:.2f}".format(
+          gamma, accuracy, auc))
+    plt.plot(fpr, tpr, label="gamma={:.3f}".format(gamma))
+plt.xlabel("FPR")
+plt.ylabel("TPR")
+plt.xlim(-0.01, 1)
+plt.ylim(0, 1.02)
+plt.legend(loc="best")
+
+
+#%% ------------------------------------
+# 多クラス分類の基準
+X_train, X_test, y_train, y_test = train_test_split(
+    digits.data, digits.target, random_state=0)
+lr = LogisticRegression().fit(X_train, y_train)
+pred = lr.predict(X_test)
+print("Accuracy: {:.3f}".format(accuracy_score(y_test, pred)))
+print("Confusion matrix:\n{}".format(confusion_matrix(y_test, pred)))
+print(classification_report(y_test, pred))
+
+# 多クラス版f-値
+# 個々のサンプルを平等に扱う
+print("Micro average f1 score: {:.3f}".format(
+    f1_score(y_test, pred, average="micro")))
+# 個々のクラスを平等に扱う
+print("Macro average f1 score: {:.3f}".format(
+    f1_score(y_test, pred, average="macro")))
+
+
+#%% ------------------------------------
+# 評価基準を用いたモデル選択
+# デフォルトのクラス分類スコアは精度
+print("Default scoring: {}".format(
+    cross_val_score(SVC(), digits.data, digits.target == 9, cv=5)))
+# scoring="accuracy" としても変わらない
+explicit_accuracy =  cross_val_score(SVC(), digits.data, digits.target == 9,
+                                     scoring="accuracy", cv=5)
+print("Explicit accuracy scoring: {}".format(explicit_accuracy))
+# scoring = roc_aucに変更
+roc_auc =  cross_val_score(SVC(), digits.data, digits.target == 9,
+                           scoring="roc_auc", cv=5)
+print("AUC scoring: {}".format(roc_auc))
+
+
+#%% 同様にGridSearchCVでも評価基準を変更できる
+X_train, X_test, y_train, y_test = train_test_split(
+    digits.data, digits.target == 9, random_state=0)
+
+# あまり良くないグリッドを指定する
+param_grid = {'gamma': [0.0001, 0.01, 0.1, 1, 10]}
+#%% デフォルトで評価
+grid = GridSearchCV(SVC(), param_grid=param_grid)
+grid.fit(X_train, y_train)
+print("Grid-Search with accuracy")
+print("Best parameters:", grid.best_params_)
+print("Best cross-validation score (accuracy)): {:.3f}".format(grid.best_score_))
+print("Test set AUC: {:.3f}".format(
+    roc_auc_score(y_test, grid.decision_function(X_test))))
+print("Test set accuracy: {:.3f}".format(grid.score(X_test, y_test)))
+
+#%% AUCスコアで評価
+grid = GridSearchCV(SVC(), param_grid=param_grid, scoring="roc_auc")
+grid.fit(X_train, y_train)
+print("\nGrid-Search with AUC")
+print("Best parameters:", grid.best_params_)
+print("Best cross-validation score (AUC): {:.3f}".format(grid.best_score_))
+print("Test set AUC: {:.3f}".format(
+    roc_auc_score(y_test, grid.decision_function(X_test))))
+print("Test set accuracy: {:.3f}".format(grid.score(X_test, y_test)))
